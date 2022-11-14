5d953b661e4e7de45c161f787138e5f299d7586a
hericklima22 <herick.lima77@gmail.com>
2022-11-14 16:48:42 -0300

    arquivo inicial com algumas cores do pyagame e as importaçõs

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+import random
+
+BLACK = (0, 0, 0)
+RED = (204, 20, 20)
+WHITE = (255, 255, 255)
+COR_INICIAL = WHITE
+
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
