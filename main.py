e47c999b93f6644233928ba6cd3d54aa0b5ff023
hericklima22 <herick.lima77@gmail.com>
2022-11-17 16:28:42 -0300

    funcao de escolha de vizinhos

--- main.py
+++ main.py
@@ -24,6 +24,51 @@ class Vortex:
       self.color = color
     pygame.draw.rect(display, color, (self.x, self.y, self.width, self.height))
 
+  def discover_neightbours(self):
+    w = self.display.get_width()
+    h = self.display.get_height()
+
+    if (self.x > 0 and self.x < w -1) and (self.y > 0 and self.y < h - 1):
+      self.neighbours.append((self.x + 1, self.y)) # vizinho da direita
+      self.neighbours.append((self.x - 1, self.y)) # vizinho da esquerda
+      self.neighbours.append((self.x, self.y + 1)) # vizinho de baixo
+      self.neighbours.append((self.x, self.y - 1)) # vizinho de cima
+    else:
+      if self.x == 0:
+        if self.y > 0 and self.y < h - 1:
+          self.neighbours.append((self.x, self.y + 1)) # vizinho de baixo
+          self.neighbours.append((self.x, self.y - 1)) # vizinho de cima
+          self.neighbours.append((self.x + 1, self.y)) # vizinho da direita
+      if self.x == w - 1:
+        if self.y > 0 and self.y < h - 1:
+          self.neighbours.append((self.x, self.y + 1)) # vizinho de baixo
+          self.neighbours.append((self.x, self.y - 1)) # vizinho de cima
+          self.neighbours.append((self.x - 1, self.y)) # vizinho da esquerda
+      if self.y == 0:
+        if self.x > 0 and self.x < w - 1:
+          self.neighbours.append((self.x, self.y + 1)) # vizinho de baixo
+          self.neighbours.append((self.x - 1, self.y)) # vizinho da esquerda
+          self.neighbours.append((self.x + 1, self.y)) # vizinho da direita
+      if self.y == h - 1:
+        if self.x > 0 and self.x < w - 1:
+          self.neighbours.append((self.x - 1, self.y)) # vizinho da esquerda
+          self.neighbours.append((self.x + 1, self.y)) # vizinho da direita
+          self.neighbours.append((self.x, self.y - 1)) # vizinho de cima
+
+      '''CANTOS'''
+      if self.x == 0 and self.y == 0:
+        self.neighbours.append((self.x, self.y + 1)) # vizinho de baixo
+        self.neighbours.append((self.x + 1, self.y)) # vizinho da direita
+      if self.x == w - 1 and self.y == 0:
+        self.neighbours.append((self.x, self.y + 1)) # vizinho de baixo
+        self.neighbours.append((self.x - 1, self.y)) # vizinho da esquerda
+      if self.x == 0 and self.y == h - 1:
+        self.neighbours.append((self.x + 1, self.y)) # vizinho da direita
+        self.neighbours.append((self.x, self.y - 1)) # vizinho de cima
+      if self.x == w - 1 and self.y == h - 1:
+        self.neighbours.append((self.x, self.y - 1)) # vizinho de cima
+        self.neighbours.append((self.x - 1, self.y)) # vizinho da esquerda
+
 WIDTH = 720
 HEIGHT = 480
 BLOCK_SIZE = 1
@@ -32,6 +77,7 @@ FPS = 1
 pygame.init()
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 display.fill(BLACK)
+print(display.get_width(), display.get_height())
 clock = pygame.time.Clock()
 pygame.display.set_caption("PixelArt")
 
@@ -42,22 +88,15 @@ def escolhe_cor(cor):
   nova_cor = list(cor)
   limite = 256
 
-  parte = max(nova_cor)
-
   if cor[0] > cor[1] and cor[0] > cor[2]: # elemento 0 é o maior da lista
     nova_cor[0] = (cor[0] + peso_cor) % limite
-  elif cor[1] > cor[0] and cor[1] > cor[2]: # elemento 1 é o maior da lista 
+  if cor[1] > cor[0] and cor[1] > cor[2]: # elemento 1 é o maior da lista 
     nova_cor[1] = (cor[1] + peso_cor) % limite
-  elif cor[2] > cor[1] and cor[2] > cor[0]: # elemento 2 é o maior da lista 
+  if cor[2] > cor[1] and cor[2] > cor[0]: # elemento 2 é o maior da lista 
     nova_cor[2] = (cor[2] + peso_cor) % limite
   else:
     i = random.randrange(0, 3)
     nova_cor[i] = (cor[i] + 2) % limite
-
-  # print((parte + peso_cor))
-  # nova_cor[nova_cor.index(parte)] = (parte + peso_cor) % limite
-
-  # print(parte)
 
   return tuple(nova_cor)
 
@@ -65,23 +104,27 @@ for i in range(WIDTH):
   cols = []
   for j in range(HEIGHT):
     cols.append(Vortex(i, j, BLOCK_SIZE, BLOCK_SIZE, display))
+    cols[j].discover_neightbours()
   vertices.append(cols)
 
 
-cor_anterior = (55,0,100)
+cor_anterior = (45,70,100)
 
+print(vertices[0][HEIGHT - 1].neighbours)
 
 def draw_field(w, h):
   global cor_anterior
   for i in range(0, w, BLOCK_SIZE):
     for j in range(0, h, BLOCK_SIZE):
       cor = escolhe_cor(cor_anterior)
-      print(cor)
+      # print(cor)
       cor_anterior = cor
       vertices[i][j].vortex(display, color=cor)
+      pygame.display.update()
     # sys.exit()
 
-
+def bfs(graph, g):
+  pass
 
 while True:
   draw_field(WIDTH, HEIGHT)
@@ -95,5 +138,5 @@ while True:
       pygame.quit()
       sys.exit()
 
-  pygame.display.update()
+  # pygame.display.update()
 
