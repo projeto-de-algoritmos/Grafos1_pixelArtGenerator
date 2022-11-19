34c04664d42dd1fdfc33de93e801b5c853db19d9
hericklima22 <herick.lima77@gmail.com>
2022-11-19 11:53:44 -0300

    implementada a BFS mas algoritmo de cor esta ruim

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import sys
 import random
 
 """CONSTANTES"""
+WIDTH = 720
+HEIGHT = 480
 BLACK = (0, 0, 0)
 RED = (204, 20, 20)
 WHITE = (255, 255, 255)
 COR_INICIAL = WHITE
-WIDTH = 720
-HEIGHT = 480
-BLOCK_SIZE = 1
-FPS = 30
+BLOCK_SIZE = 3  # DEIXAR ENTRE 1 E 5 PARA FICAR MAIS BONITO
+FPS = 300       # VELOCIDADE DOS PIXELS
 vertices = []
+visitados = []
+fila = []
 cor_anterior = (random.randrange(256),random.randrange(256),random.randrange(256))
 
 pygame.init()
@@ -30,6 +32,8 @@ class Vortex:
     self.display = display
     self.neighbours = []
     self.is_vortex = True
+    self.visited = False
+    self.is_wall()
 
   def vortex(self, display, color=None):
     if color is None or not self.is_vortex:
@@ -41,17 +45,20 @@ class Vortex:
   def is_wall(self):
     w = self.display.get_width()
     h = self.display.get_height()
+    y_cond = self.y == 0 or self.y >= h - 1
+    x_cond = self.x == 0 or self.x >= w - 1
 
-    if self.x == 0 or self.x == w - 1 or self.y == 0 or self.y == h - 1:
+    if x_cond or y_cond:
       self.is_vortex = False
       self.color = BLACK
       self.neighbours = []
 
-  def discover_neightbours(self, field):
+  def discover_neighbours(self, field):
     w = self.display.get_width()
     h = self.display.get_height()
 
-    if (self.x > 0 and self.x < w -1) and (self.y > 0 and self.y < h - 1):
+    if (self.x > 0 and self.x < w - 1) and (self.y > 0 and self.y < h - 1):
+      # print('entrou')
       if field[self.x + 1][self.y].is_vortex:
         self.neighbours.append(field[self.x + 1][self.y]) # vizinho da direita
       if field[self.x - 1][self.y].is_vortex:
@@ -62,7 +69,7 @@ class Vortex:
         self.neighbours.append(field[self.x][self.y - 1]) # vizinho de cima
 
 def escolhe_cor(cor):
-  peso_cor = 3
+  peso_cor = 1
   nova_cor = list(cor)
   limite = 256
 
@@ -82,12 +89,12 @@ for i in range(WIDTH):
   cols = []
   for j in range(HEIGHT):
     cols.append(Vortex(i, j, BLOCK_SIZE, BLOCK_SIZE, display))
-    cols[j].is_wall()
+    # cols[j].is_wall()
   vertices.append(cols)
 
 for i in range(WIDTH):
   for j in range(HEIGHT):
-    vertices[i][j].discover_neightbours(vertices)
+    vertices[i][j].discover_neighbours(vertices)
 
 def draw_field(w, h):
   global cor_anterior
@@ -96,13 +103,35 @@ def draw_field(w, h):
       cor = escolhe_cor(cor_anterior)
       cor_anterior = cor
       vertices[i][j].vortex(display, color=cor)
-      # pygame.display.update() # manter comentado se quiser atualizar a tela toda de uma vez (se tirar o comentario, sera atualizado pixel por pixel)
+      clock.tick(FPS)
+      pygame.display.update() # manter comentado se quiser atualizar a tela toda de uma vez (se tirar o comentario, sera atualizado pixel por pixel)
 
-def bfs(graph, g):
-  pass
+def bfs(graph, queue, node):  # recebe a lista de vertices
+  global cor_anterior
+  node.visited = True
+  node.vortex(display, color=COR_INICIAL)
+  queue.append(node)
+  
+  while queue:
+    s = queue.pop(0)
+    # print(s.neighbours)
+
+    for n in s.neighbours:
+      if not n.visited:
+        n.visited = True
+        queue.append(n)
+        cor = escolhe_cor(cor_anterior)
+        cor_anterior = cor
+        n.vortex(display, color=cor)
+        pygame.display.update()
+
+
+
+
 
 while True:
-  draw_field(WIDTH, HEIGHT)
+  # draw_field(WIDTH, HEIGHT)
+  bfs(vertices, fila, vertices[1][1])
   clock.tick(FPS)
 
   for event in pygame.event.get():
@@ -113,5 +142,5 @@ while True:
       pygame.quit()
       sys.exit()
 
-  pygame.display.update()
+  # pygame.display.update()
 
